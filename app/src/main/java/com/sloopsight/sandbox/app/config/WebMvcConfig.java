f1646d62903bc78e2d35a33fda435b75db0618b8
slooper <slooper@sloopsight.com>
2020-10-27 19:12:24 +0530

    Bug fixes

--- app/src/main/java/com/sloopsight/sandbox/app/config/WebMvcConfig.java
+++ app/src/main/java/com/sloopsight/sandbox/app/config/WebMvcConfig.java
@@ -21,7 +21,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         registry.addResourceHandler("/**").addResourceLocations("classpath:/")
-                .setCacheControl(CacheControl.maxAge(31, TimeUnit.DAYS)).resourceChain(true)
+            .setCacheControl(CacheControl.maxAge(2, TimeUnit.DAYS).mustRevalidate().noTransform()).resourceChain(true)
                 .addResolver(new PathResourceResolver() {
                     @Override
                     protected Resource getResource(String resourcePath, Resource location) throws IOException {
